f84dda1a8662cc5b401c30944b531cc3669c5a31
NickhailaLodge <nickhailalodge204@gmail.com>
2022-11-13 19:55:46 -0500

    Edited world.php

--- world.php
+++ world.php
@@ -1,17 +1,70 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+function getQuery($conn){
+
+  $country = filter_var(isset($_GET['country']), FILTER_SANITIZE_STRING);
+  $city = filter_var(isset($_GET['context']),FILTER_SANITIZE_STRING);
+
+  if($city === "cities"){  
+    $stmt = $conn -> query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON country_code = countries.code WHERE countries.name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $theader = "";
+    $theader .= "<tr>";
+    $theader .= "<th>Name</th>";
+    $theader .= "<th>District</th>";
+    $theader .= "<th>Population</th>";
+    $theader .= "</tr>";
+
+    foreach ($results as $row){
+      $tdata = "";
+      $tdata .= "<tr>";
+      $tdata .= "<td>".$row['name']."</td>";
+      $tdata .= "<td>".$row['district']."</td>";
+      $tdata .= "<td>".$row['population']."</td>";
+      $tdata .= "</tr>";
+    }
+
+    echo "<table>";
+    echo $theader. " ". $tdata;
+    echo "$</table>";
+
+
+  }else{
+    $stmt = $conn -> query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $theader = "";
+    $theader .= "<tr>";
+    $theader .= "<th>Name</th>";
+    $theader .= "<th>Continent</th>";
+    $theader .= "<th>Independence Year</th>";
+    $theader .= "<th>Head of State</th>";
+    $theader .= "</tr>";
+
+    foreach ($results as $row){
+      $tdata = "";
+      $tdata .= "<tr>";
+      $tdata .= "<td>".$row['name']."</td>";
+      $tdata .= "<td>".$row['continent']."</td>";
+      $tdata .= "<td>".$row['independence_year']."</td>";
+      $tdata .= "<td>".$row['head_of_state']."</td>";
+      $tdata .= "</tr>";
+    }
+
+    echo "<table>";
+    echo $theader. " ".$tdata;
+    echo "$</table>";
+  }
+}
+getQuery($conn);
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
